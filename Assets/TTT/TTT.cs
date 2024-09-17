efd5d7fc0bdad820bbd76eca3a99f656134cfc20
Tina <143371814+Trinketina@users.noreply.github.com>
2024-09-17 16:21:06 -0500

    i think it works?
    This thing is so incredibly over engineered

--- Assets/TTT/TTT.cs
+++ Assets/TTT/TTT.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Unity.VisualScripting;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public enum PlayerOption
 {
@@ -38,6 +40,11 @@ public class TTT : MonoBehaviour
                 cells[j, i].current = PlayerOption.NONE;
             }
         }
+    }
+
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
     }
 
     private Vector2Int GetPosition(int index)
@@ -104,24 +111,78 @@ public class TTT : MonoBehaviour
              * - O -
              * - - - 
              */
+            if (cells[0, 1].current == PlayerOption.X)
+            {
+                ChooseSpace(0, 0);
+                return;
+            }
+            else if (cells[1, 0].current == PlayerOption.X)
+            {
+                ChooseSpace(0, 0);
+                return;
+            }
+            else if (cells[2, 1].current == PlayerOption.X) 
+            {
+                ChooseSpace(2, 2);
+                return;
+            }
+            else if (cells[1, 2].current == PlayerOption.X)
+            {
+                ChooseSpace(2, 2);
+                return;
+            }
+
             if (ChooseSpace(1, 1))
                 return;
         }
 
-        //Check if ai can win immediately
         Vector2Int bestMove = new();
         int bestSum = 0;
-        for (int i = 0; i < Rows; i++)
+
+        //edge case for unoptimal O placement
+        if (turn == 2 && (lastXPosition.x + lastXPosition.y) % 2 == 0 && (lastOPosition.x + lastOPosition.y) % 2 == 0)
         {
-            for (int j = 0; j < Columns; j++)
+            Debug.Log(true);
+            int sum = CalculateStraightValues(0, 0);
+            if (sum > bestSum)
             {
-                if (TryAIPosition(j, i, currentPlayer))
-                {
-                    Debug.Log("win/stopwin");
-                    ChooseSpace(j, i);
-                    return;
-                }
+                bestSum = sum;
+                bestMove = new(0, 0);
             }
+            sum = CalculateStraightValues(2, 0);
+            if (sum > bestSum)
+            {
+                bestSum = sum;
+                bestMove = new(2, 0);
+            }
+            sum = CalculateStraightValues(0, 2);
+            if (sum > bestSum)
+            {
+                bestSum = sum;
+                bestMove = new(0, 2);
+            }
+            sum = CalculateStraightValues(2, 2);
+            if (sum > bestSum)
+            {
+                bestSum = sum;
+                bestMove = new(2, 2);
+            }
+
+            ChooseSpace(bestMove.x, bestMove.y);
+            return;
+        }
+        //Check if ai can win immediately
+        for (int i = 0; i < Rows*Columns; i++)
+        {
+            Vector2Int move = GetPosition(i);
+            if (TryAIPosition(move.x, move.y, currentPlayer))
+            {
+                Debug.Log(move);
+                Debug.Log("win/stopwin");
+                ChooseSpace(move.x, move.y);
+                return;
+            }
+            
         }
 
         //Get opponent
@@ -131,121 +192,47 @@ public class TTT : MonoBehaviour
         else
             counterPlayer = PlayerOption.O;
         // Check if ai can block a win
-        for (int i = 0; i < Rows; i++)
-        {
-            for (int j = 0; j < Columns; j++)
-            {
-                if (TryAIPosition(j, i, counterPlayer))
-                {
-                    Debug.Log("win/stopwin");
-                    ChooseSpace(j, i);
-                    return;
-                }
-                if (CalculateMoveValue(j, i) > bestSum)
-                    bestMove = new(j, i);
-            }
-        }
-        ChooseSpace(bestMove.x, bestMove.y);
-        return;
 
-        if (turn == 2)
+        for (int i = 0; i < Rows * Columns; i++)
         {
-            if (lastOPosition == new Vector2Int(0, 0))
+            Vector2Int move = GetPosition(i);
+            if (TryAIPosition(move.x, move.y, counterPlayer))
             {
-                ChooseSpace(lastXPosition.y, lastXPosition.x);
+                Debug.Log(move);
+                Debug.Log("win/stopwin");
+                ChooseSpace(move.x, move.y);
+                return;
             }
+        }
 
-        }
-/*        if (turn == 2)
+        if (turn == 3)
         {
-            if (cells[1,1].current == PlayerOption.O)
-            {
-                if (cells[0, 2].current == PlayerOption.X)
-                {
-                    ChooseSpace(2, 0);
-                    return;
-                }
-                if (cells[2, 0].current == PlayerOption.X)
-                {
-                    ChooseSpace(0, 2);
-                    return;
-                }
-                if (cells[2, 2].current == PlayerOption.X)
-                {
-                    ChooseSpace(0, 0);
-                    return;
-                }
-                if (cells[0, 0].current == PlayerOption.X)
-                {
-                    ChooseSpace(2, 2);
-                    return;
-                }
-            }
-            if (cells[0, 1].current == PlayerOption.NONE && cells[1, 0].current == PlayerOption.NONE && cells[0, 0].current == PlayerOption.NONE)
-            {
-                Debug.Log(true);
-                ChooseSpace(0, 0);
-                return;
-            }
-            else if (ChooseSpace(2, 2))
-            {
-                Debug.Log(false);
-                return;
-            }
-            ChooseSpace(0, 2);
-        }
-        else if (turn == 3) //try place on edge with an X
-        {
-            if (ChooseSpace(0, 1))
-                return;
-            if (ChooseSpace(1, 0))
-                return;
-            if (ChooseSpace(1, 2))
-                return;
-            if (ChooseSpace(2, 1))
+            if (ChooseSpace(1, 1))
                 return;
         }
-        else if (turn <= 4) // try to place in corner if along 
-        {
-            if (cells[0, 0].current == PlayerOption.O || cells[2, 0].current == PlayerOption.O || cells[2, 2].current == PlayerOption.O)
-            {
-                if (ChooseSpace(0, 0))
-                    return;
-                if (ChooseSpace(2, 0))
-                    return;
-                ChooseSpace(2, 2);
-                return;
-            }
-            if (cells[1, 1].current == PlayerOption.O)
-            {
-                ChooseSpace(2, 1);
-            }
 
-            ChooseSpace(1, 1);
-        }
-        else if (turn >= 5)
+        if (turn == 4 && bestSum < 2)
         {
-            if (ChooseSpace(1, 1)) //try place middle to win
-                return;
-
-            //otherwise try place corner to win
-            if (ChooseSpace(2, 2))
+            if (ChooseSpace(0, 0))
                 return;
             if (ChooseSpace(2, 0))
                 return;
-            if (ChooseSpace(0, 0))
+            if (ChooseSpace(2, 2))
                 return;
-
-
-            if (cells[0, 0].current == PlayerOption.X && cells[2, 0].current == PlayerOption.X)
-                ChooseSpace(1, 0);
-            else
-                ChooseSpace(1, 2);
+            if (ChooseSpace(0, 2))
+                return;
         }
-        else if (turn == 8)
-        {
 
-        }*/
+        if (ChooseSpace(bestMove.x, bestMove.y))
+            return;
+
+        //if all else fails, just loop through entire board and try to place
+        for (int i = 0; i < Rows*Columns; i++)
+        {
+            Vector2Int pos = GetPosition(i);
+            if (ChooseSpace(pos.x, pos.y))
+                return;
+        }
     }
 
     public bool ChooseSpace(int column, int row)
@@ -299,22 +286,123 @@ public class TTT : MonoBehaviour
 
         if (possibleWin != PlayerOption.NONE)
             canWin = true;
-/*        else
-        {
-            //see if any positions can win for enemy
-            if (option == PlayerOption.X)
-                cells[col, row].current = PlayerOption.O;
-            else
-                cells[col, row].current = PlayerOption.X;
-            possibleWin = GetWinner();
 
-            if (possibleWin != PlayerOption.NONE)
-                canWin = true;
-        }*/
         cells[col, row].current = PlayerOption.NONE;
         return canWin;
     }
 
+
+    public int CalculateStraightValues(int col, int row)
+    {
+        if (cells[col, row].current != PlayerOption.NONE)
+            return 0;
+
+        PlayerOption counterPlayer;
+        if (currentPlayer == PlayerOption.X)
+            counterPlayer = PlayerOption.O;
+        else
+            counterPlayer = PlayerOption.X;
+
+        // sum each row/column based on what's in each cell X = 1, O = -1, blank = 0
+        // we have a winner if the sum = 3 (X) or -3 (O)
+        int sum = 0;
+        int bestSum = 0;
+
+        // check rows
+        for (int i = 0; i < Rows; i++)
+        {
+            sum = 0;
+            for (int j = 0; j < Columns; j++)
+            {
+                var value = 0;
+                if (cells[j, i].current == currentPlayer)
+                    value = 1;
+                else if (cells[j, i].current == counterPlayer)
+                    value = -1;
+
+                sum += value;
+            }
+
+            if (sum > bestSum)
+                bestSum = sum;
+
+        }
+
+        // check columns
+        for (int j = 0; j < Columns; j++)
+        {
+            //sum = 0;
+            for (int i = 0; i < Rows; i++)
+            {
+                var value = 0;
+                if (cells[j, i].current == currentPlayer)
+                    value = 1;
+                else if (cells[j, i].current == counterPlayer)
+                    value = -1;
+
+                sum += value;
+            }
+
+            if (sum > bestSum)
+                bestSum = sum;
+
+        }
+
+        return bestSum;
+    }
+
+    public int CalculateDiagonalValues(int col, int row)
+    {
+        if (cells[col, row].current != PlayerOption.NONE)
+            return 0;
+
+        PlayerOption counterPlayer;
+        if (currentPlayer == PlayerOption.X)
+            counterPlayer = PlayerOption.O;
+        else
+            counterPlayer = PlayerOption.X;
+
+        // sum each row/column based on what's in each cell X = 1, O = -1, blank = 0
+        // we have a winner if the sum = 3 (X) or -3 (O)
+        int sum = 0;
+        int bestSum = 0;
+
+        // check diagonals
+        // top left to bottom right
+        sum = 0;
+        for (int i = 0; i < Rows; i++)
+        {
+            int value = 0;
+            if (cells[i, i].current == currentPlayer)
+                value = 1;
+            else if (cells[i, i].current == counterPlayer)
+                value = -1;
+
+            sum += value;
+        }
+
+        if (sum > bestSum)
+            bestSum = sum;
+
+        // top right to bottom left
+        sum = 0;
+        for (int i = 0; i < Rows; i++)
+        {
+            int value = 0;
+
+            if (cells[Columns - 1 - i, i].current == currentPlayer)
+                value = 1;
+            else if (cells[Columns - 1 - i, i].current == counterPlayer)
+                value = -1;
+
+            sum += value;
+        }
+
+        if (sum > bestSum)
+            bestSum = sum;
+
+        return bestSum;
+    }
     public int CalculateMoveValue(int col, int row)
     {
         if (cells[col, row].current != PlayerOption.NONE)
